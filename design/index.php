85f446149616b4c40336839e5a128639a54d66dd
Pongsathorn Kaewyangnok <pongsathorn.kaew@gmail.com>
2017-04-23 14:58:29 +0700

    Add a sub-menu example for topic apps. Start coding on content area.

--- design/index.php
+++ design/index.php
@@ -7,8 +7,22 @@
       $specail_tag = '';                               #Define special tag for page
       get_header($title,$desc,$keyword,$specail_tag);  #function get_header(Title , Desc , Keyword and Specail_tag);
 ?>
-<h2>Title</h2>
-Start code here
+<h2>ตัวอย่างเมนูย่อยโสเหล่</h2>
+<html>
+  <title>W3.CSS</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+  <div class="w3-bar-block w3-red">
+    <a href="#" class="w3-bar-item w3-button w3-text-white">รวมกระทู้</a>
+    <a href="#" class="w3-bar-item w3-button w3-text-white">เว่านัวหัวม่วน</a>
+    <a href="#" class="w3-bar-item w3-button w3-text-white">ข่าวสารบ้านเฮา</a>
+    <a href="#" class="w3-bar-item w3-button w3-text-white">ตลาดซื้อขาย</a>
+    <a href="#" class="w3-bar-item w3-button w3-text-white">กฎและกติกา</a>
+    <a href="#" class="w3-bar-item w3-button w3-text-white">แท็ก</a>
+  </div>
+</body>
+</html>
 
 <?php
   #<!-- End of your content-->
