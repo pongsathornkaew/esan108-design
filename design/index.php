2c80ca62c3142fda29f12e9825d518ad3fe993f0
Green Hub <pongpat4111@gmail.com>
2017-04-23 16:16:42 +0700

    Update menu sole

--- design/index.php
+++ design/index.php
@@ -8,22 +8,16 @@
       get_header($title,$desc,$keyword,$specail_tag);  #function get_header(Title , Desc , Keyword and Specail_tag);
 ?>
 <h2>ตัวอย่างเมนูย่อยโสเหล่</h2>
-<html>
-  <title>W3.CSS</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<body>
-  <div class="w3-bar-block w3-red">
-    <a href="#" class="w3-bar-item w3-button w3-text-white">รวมกระทู้</a>
-    <a href="#" class="w3-bar-item w3-button w3-text-white">เว่านัวหัวม่วน</a>
-    <a href="#" class="w3-bar-item w3-button w3-text-white">ข่าวสารบ้านเฮา</a>
-    <a href="#" class="w3-bar-item w3-button w3-text-white">ตลาดซื้อขาย</a>
-    <a href="#" class="w3-bar-item w3-button w3-text-white">กฎและกติกา</a>
-    <a href="#" class="w3-bar-item w3-button w3-text-white">แท็ก</a>
+<div class="w3-row">
+  <div class="w3-col s12 m6 l12">
+      <div class="w3-button w3-round w3-white w3-hover-red w3-margin-right w3-padding w3-hover-text-white"><a href="#">รวมกระทู้</a></div>
+      <div class="w3-button w3-round w3-white w3-hover-red w3-margin-right w3-padding w3-hover-text-white"><a href="#" >เว่านัวหัวม่วน</a></div>
+      <div class="w3-button w3-round w3-white w3-hover-red w3-margin-right w3-padding w3-hover-text-white"><a href="#" >ข่าวสารบ้านเฮา</a></div>
+      <div class="w3-button w3-round w3-white w3-hover-red w3-margin-right w3-padding w3-hover-text-white"><a href="#" >ตลาดซื้อขาย</a></div>
+      <div class="w3-button w3-round w3-white w3-hover-red w3-margin-right w3-padding w3-hover-text-white"><a href="#" >กฎและกติกา</a></div>
+      <div class="w3-button w3-round w3-white w3-hover-red w3-margin-right w3-padding w3-hover-text-white"><a href="#" >แท็ก</a></div>
   </div>
-</body>
-</html>
-
+</div>
 <?php
   #<!-- End of your content-->
   get_sidebar(''); //Show side bar  ==> user , dic , wiki , topic etc. (Define in ../includes/main-config.php)
